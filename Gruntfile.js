fb39d608b36fb176049517f10f6b7cd1c978d704
Qianglong Mo <sammok2003@hotmail.com>
2015-04-01 17:33:46 +0800

    Init repos.

---
+++ Gruntfile.js
@@ -0,0 +1,143 @@
+// develop path: "./"
+// build path:  "dist"
+
+// task order
+//- Clean dist directory
+//- Copy file to dist directory
+//- Compile less, sass, coffee script into dist directory
+//- Concat css, js both to be single file in dist directory
+//- Compressor css and js file in dist directory
+//- Injector css and js file to html
+
+module.exports = function (grunt){
+    // auto-load npm task components
+    require("load-grunt-tasks")(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('./package.json'),
+
+        // clean directory
+        clean: {
+            build: ["dist"]
+        },
+
+        // copy file to dist directory
+        copy: {
+            build: {
+                files: [
+                    {
+                        src: [ 'assets/scripts/*', '*.html'],
+                        dest: 'dist/',
+                        expand: true
+                    }
+                ]
+            }
+        },
+
+        // less compiler
+        less: {
+            build: {
+                expand: true,
+                cwd: 'assets/stylesheets/',
+                src: ['*.less'],
+                dest: 'dist/assets/stylesheets',
+                ext: '.css'
+            }
+        },
+
+        // concat and compressor css
+        cssmin: {
+            build: {
+                files: [{
+                    'dist/assets/stylesheets/main.min.css': ['assets/stylesheets/*.css', 'dist/assets/stylesheets/*.css']
+                }]
+            }
+        },
+
+        // concat js
+        concat: {
+            build: {
+                src:'assets/scripts/index.js',
+                dest:'dist/assets/scripts/index.js'
+            }
+        },
+
+        // compressor js
+        uglify: {
+            build: {
+                src:['assets/scripts/*.js', '!assets/scripts/less.min.js'],
+                dest:'dist/assets/scripts/main.min.js'
+            }
+        },
+
+        // injector js and css files to html
+        injector: {
+            options: {
+                // Task-specific options go here.
+            },
+            css: {
+                options: {
+                    relative: true,
+                    transform: function (filePath){
+                        var filePath = filePath.replace('/dist/', '');
+                        return '<link rel="stylesheet" href="' + filePath + '" />';
+                    },
+                    starttag: '<!-- injector:css -->',
+                    endtag: '<!-- endinjector -->',
+                },
+                files: {
+                    'dist/index.html': ['dist/assets/stylesheets/*.css']
+                }
+            },
+            scripts: {
+                options: {
+                    relative: true,
+                    transform: function (filePath){
+                        var filePath = filePath.replace('/dist/', '');
+                        return '<script src="' + filePath + '"></script>';
+                    },
+                    starttag: '<!-- injector:js -->',
+                    endtag: '<!-- endinjector -->'
+                },
+                files: {
+                    'dist/index.html': ['dist/assets/scripts/*.js']
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: 'assets/stylesheets/**',
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: 'assets/script/**',
+                options: {
+                    livereload: true
+                }
+            },
+            html: {
+                files: ['*.html', '**/*.html', '**/**/*.html'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    // define task
+    grunt.registerTask('cleanDir', ['clean:build']); //ok
+    grunt.registerTask('copyFileToDist', ['copy:build']); //ok
+    grunt.registerTask('compileLess', ['less:build']); //ok
+    grunt.registerTask('concatCompressorCss', ['cssmin:build']); //ok
+    grunt.registerTask('makeJs', ['concat:build', 'uglify:build']); //ok
+    grunt.registerTask('injectFileToHtml', ['injector']); //ok
+    // main task
+    //grunt.registerTask('deploy', ['cleanDir', 'copyFileToDist', 'compileLess', 'concatCompressorCss', 'makeJs', 'injectFileToHtml']);
+    grunt.registerTask('deploy', ['cleanDir', 'copyFileToDist', 'compileLess', 'injectFileToHtml']);
+    grunt.registerTask('live', ['watch']);
+
+
+};
---
+++ assets/scripts/main.js
@@ -0,0 +1,22 @@
+// Created by sam mok 2015(Siso brand interactive team).
+
+"use strict";
+
+var app = {
+    config: function (){
+
+    },
+
+    create: function (){
+
+    },
+
+    start: function (){
+        this.config();
+    }
+};
+
+$(function (){
+    // init app
+    app.start();
+});
